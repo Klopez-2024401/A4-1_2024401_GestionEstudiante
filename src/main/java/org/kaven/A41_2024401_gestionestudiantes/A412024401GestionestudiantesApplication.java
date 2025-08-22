455145f85e17ef1df8d62757fcd2083e4b67d8bd
JavierGomez0 <jgomez-2024026@kinal.edu.gt>
2025-08-22 16:08:56 -0600

    se guarto todo

---
+++ src/main/java/org/kaven/A41_2024401_gestionestudiantes/A412024401GestionestudiantesApplication.java
@@ -0,0 +1,13 @@
+package org.kaven.A41_2024401_gestionestudiantes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class A412024401GestionestudiantesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(A412024401GestionestudiantesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/kaven/A41_2024401_gestionestudiantes/A412024401GestionestudiantesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.kaven.A41_2024401_gestionestudiantes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class A412024401GestionestudiantesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
